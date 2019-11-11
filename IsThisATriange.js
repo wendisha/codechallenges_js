aa05fa152f1beea101a94b3b0f97eaf5d41f7e65
Wendy Calderon <wendycalderon@gmail.com>
2019-11-11 11:58:36 -0500

    isTriange (Codewars) - Solution

---
+++ IsThisATriange.js
@@ -0,0 +1,13 @@
+//IF any two sides sum is greater than the third side
+//  return true
+//ELSE 
+//  return false
+
+function isTriangle(a,b,c)
+{
+  if(a+b > c && a+c > b && b+c > a){
+    return true;
+  }else{
+   return false;
+  }
+}
